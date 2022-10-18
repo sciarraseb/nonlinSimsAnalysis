0fcc28a845affd51da7fab597786f0e4ac286e07
Seb Sciarra <ssciarra@uoguelph.ca>
2022-10-18 11:51:17 -0400

    Generic function.

---
+++ R/generic_function.R
@@ -0,0 +1,7 @@
+#' Computes summary data for each experimental conition/cell.
+#'
+#' @param param_summary_data parameter summary data (created from compute_parameter_summary)
+#' @export
+generic_function <- function(){
+  print('this')
+}
