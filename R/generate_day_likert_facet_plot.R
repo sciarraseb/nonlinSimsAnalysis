d12ce08eb4beb4efb783691d092be3be0b54b211
Seb Sciarra <ssciarra@uoguelph.ca>
2022-10-21 12:29:54 -0400

    update.

--- R/generate_day_likert_facet_plot.R
+++ R/generate_day_likert_facet_plot.R
@@ -2,6 +2,7 @@
 #'
 #' @param analytical_data experiment data
 #' @param target_col experimental conditions
+#' @return ggplot
 #' @export
 generate_day_likert_facet_plot <- function(analytical_data, target_col = 'measurement_spacing',
                                            target_value, x_axis_var, x_axis_name, exp_num,
