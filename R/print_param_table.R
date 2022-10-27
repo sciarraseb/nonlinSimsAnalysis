304eb4c03566da96a95f8a1ae663f5a7f53b29f6
Seb Sciarra <ssciarra@uoguelph.ca>
2022-10-27 19:26:51 -0400

    Param table update.

--- R/print_param_table.R
+++ R/print_param_table.R
@@ -2,7 +2,6 @@
 #'
 #' @param param_summary_data parameter summary data (created from compute_parameter_summary)
 #' @export
-#extracts values for the fixed- and random-effect estimates for one parameter and produces an APA table
 print_param_table <- function(table_ready_data, parameter_name,
                               caption_name, col_header_name, IV_names, column_names, footnote
                               ) {
